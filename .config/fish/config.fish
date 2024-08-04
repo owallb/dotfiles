97539a8878c4eb1460b7ab94fe29cdd57c586872
Oscar Wallberg <oscar.wallberg@pm.me>
2024-08-04 14:30:23 +0200

    feat(fish): add some config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,13 @@
-if status is-interactive
-    # Commands to run in interactive sessions can go here
+set -gx PATH \
+    $HOME/.local/bin \
+    $HOME/go/bin \
+    /usr/local/bin \
+    /usr/bin
+set -gx VISUAL nvim
+set -gx EDITOR nvim
+set -gx DIFFPROG nvim -d
+
+if not status is-interactive
+    return
 end
+
